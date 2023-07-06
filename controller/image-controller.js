2e59680e2a138bc1d7897790b73e5bf4b590166f
Shivam kumar <86045489+shivam1484@users.noreply.github.com>
2023-07-07 00:13:07 +0530

    fixed

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -14,7 +14,7 @@ export const uploadImage = async (request, response) => {
     const file = await File.create(fileObj);
     response
       .status(200)
-      .json({ path: `http://localhost:${process.env.PORT}/file/${file._id}` });
+      .json({ path: `https://fsssss.onrender.com/file/${file._id}` });
   } catch (error) {
     console.error(error.message);
     response.status(500).json({ error: error.message });
